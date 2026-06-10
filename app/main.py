1fd082d0ad6106d52803ea3bb420e2c5bab3b099
Shubhankar Mathur <mathurshubhankar@gmail.com>
2026-06-10 16:48:30 +0530

    Add FastAPI runtime v1 scaffolding

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+from app.api.health import router as health_router
+from app.api.runtime import router as runtime_router
+
+
+app = FastAPI()
+
+app.include_router(health_router)
+app.include_router(runtime_router)
